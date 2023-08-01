63384da728b3deada1dc333feef2199e9283802d
ThomasF85 <thomas.foeldi@gmail.com>
2023-08-01 13:35:29 +0200

    add next auth

--- pages/api/fish/[id].js
+++ pages/api/fish/[id].js
@@ -1,13 +1,27 @@
 import fishData from "@/db/fish.json";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { getServerSession } from "next-auth/next";
 
 async function handler(req, res) {
   const { id } = req.query;
+
   switch (req.method) {
     case "GET":
       try {
+        const session = await getServerSession(req, res, authOptions);
+        // check if a user is logged in
+        if (!session) {
+          return res.status(401).json({ msg: "please log in" });
+        }
         const fish = fishData.find((fish) => fish.id === id);
         if (!fish) {
           return res.status(404).json({ message: "fish not found" });
+        }
+        // check if user email is the same as in fish data
+        if (session.user.email !== fish.email) {
+          return res
+            .status(401)
+            .json({ msg: "you are not allowed to see this fish" });
         }
         return res.status(200).json(fish);
       } catch (error) {
